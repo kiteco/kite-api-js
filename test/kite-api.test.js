6e7894d31164172585869baea00c7c3ba08f4813
abe33 <cedric.nehemie@gmail.com>
2018-04-26 15:13:33 +0200

    Start bringing back auth methods from StateController

--- test/kite-api.test.js
+++ test/kite-api.test.js
@@ -4,6 +4,8 @@ const sinon = require('sinon');
 const expect = require('expect.js');
 const KiteConnector = require('kite-connect');
 const {withKite, withKiteRoutes} = require('kite-connect/test/helpers/support');
+const {waitsForPromise} = require('kite-connect/test/helpers/async');
+const {fakeResponse} = require('kite-connect/test/helpers/http');
 
 const KiteAPI = require('../lib');
 
@@ -32,9 +34,98 @@ describe('KiteAPI', () => {
     'isUserAuthenticated',
   ].forEach(method => {
     it(`delegates calls to ${method} to the connector`, () => {
-      sinon.stub(KiteConnector, method).callsFake(() => {});
+      const stub = sinon.stub(KiteConnector, method).callsFake(() => {});
       KiteAPI[method]('foo', 'bar');
       expect(KiteConnector[method].calledWith('foo', 'bar')).to.be.ok();
+      stub.restore();
+    });
+  });
+
+  describe('.canAuthenticateUser()', () => {
+    withKite({reachable: false}, () => {
+      it('returns a rejecting promise', () => {
+        return waitsForPromise({shouldReject: true}, () => KiteAPI.canAuthenticateUser());
+      });
+    });
+
+    withKite({logged: true}, () => {
+      it('returns a rejecting promise', () => {
+        return waitsForPromise({shouldReject: true}, () => KiteAPI.canAuthenticateUser());
+      });
+    });
+
+    withKite({logged: false}, () => {
+      it('returns a resolving promise', () => {
+        return waitsForPromise(() => KiteAPI.canAuthenticateUser());
+      });
+    });
+  });
+
+  describe('.authenticateUser()', () => {
+    withKite({reachable: false}, () => {
+      it('returns a rejected promise', () => {
+        waitsForPromise({shouldReject: true}, () => KiteAPI.canAuthenticateUser());
+      });
+    });
+
+    withKite({reachable: true}, () => {
+      describe('and the authentication succeeds', () => {
+        withKiteRoutes([[
+          o => o.path === '/api/account/login',
+          o => fakeResponse(200, 'authenticated'),
+        ]]);
+
+        it('returns a resolving promise', () => {
+          return waitsForPromise(() =>
+            KiteAPI.authenticateUser('email', 'password'));
+        });
+      });
+
+      describe('and the authentication fails', () => {
+        withKiteRoutes([[
+          o => o.path === '/api/account/login',
+          o => fakeResponse(401),
+        ]]);
+
+        it('returns a rejected promise', () => {
+          return waitsForPromise({shouldReject: true}, () =>
+            KiteAPI.authenticateUser('email', 'password'));
+        });
+      });
+    });
+  });
+
+  describe('.authenticateSessionID()', () => {
+    withKite({reachable: false}, () => {
+      it('returns a rejected promise', () => {
+        return waitsForPromise({shouldReject: true}, () => KiteAPI.canAuthenticateUser());
+      });
+    });
+
+    withKite({reachable: true}, () => {
+      describe('and the authentication succeeds', () => {
+        withKiteRoutes([[
+          o => /^\/api\/account\/authenticate/.test(o.path),
+          o => fakeResponse(200, 'authenticated'),
+        ]]);
+
+        it('returns a resolving promise', () => {
+          return waitsForPromise(() =>
+            KiteAPI.authenticateSessionID('key'));
+        });
+      });
+
+      describe('and the authentication fails', () => {
+        withKiteRoutes([[
+          o => /^\/api\/account\/authenticate/.test(o.path),
+          o => fakeResponse(401),
+        ]]);
+
+        it('returns a rejected promise', () => {
+          return waitsForPromise({shouldReject: true}, () =>
+            KiteAPI.authenticateSessionID('key'));
+        });
+      });
     });
   });
 });
