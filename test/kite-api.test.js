9cacfa58eefd4028d88d81c2d6c764602592423f
abe33 <cedric.nehemie@gmail.com>
2018-05-07 12:15:05 +0200

    Add remaining API methods from plugin's DataLoader

--- test/kite-api.test.js
+++ test/kite-api.test.js
@@ -295,8 +295,8 @@ describe('KiteAPI', () => {
     });
   });
 
-  describe('.getSupportedLanguages()', () => {
-    withKite({logged: true}, () => {
+  withKite({logged: true}, () => {
+    describe('.getSupportedLanguages()', () => {
       withKiteRoutes([[
         o => o.path === '/clientapi/languages',
         o => fakeResponse(200, JSON.stringify(['javascript', 'python'])),
@@ -309,12 +309,11 @@ describe('KiteAPI', () => {
         });
       });
     });
-  });
 
-  describe('.getHoverDataAtPosition()', () => {
-    const source = loadFixture('sources/json-dump.py');
-    const filename = '/path/to/json-dump.py';
-    withKite({logged: true}, () => {
+    describe('.getHoverDataAtPosition()', () => {
+      const source = loadFixture('sources/json-dump.py');
+      const filename = '/path/to/json-dump.py';
+
       describe('when the request succeeds', () => {
         withKiteRoutes([[
           o => /^\/api\/buffer\/atom/.test(o.path),
@@ -350,6 +349,307 @@ describe('KiteAPI', () => {
         it('returns a rejected promise', () => {
           return waitsForPromise({shouldReject: true}, () =>
             KiteAPI.getHoverDataAtPosition(filename, source, 18));
+        });
+      });
+
+    });
+
+    describe('.getReportDataAtPosition()', () => {
+      const source = loadFixture('sources/json-dump.py');
+      const filename = '/path/to/json-dump.py';
+
+      describe('when the hover request succeeds but not the report request', () => {
+        withKiteRoutes([
+          [
+            o => /^\/api\/buffer\/atom/.test(o.path),
+            o => fakeResponse(200, JSON.stringify({
+              symbol: [{
+                id: 'foo',
+                value: [],
+              }],
+            })),
+          ], [
+            o => /^\/api\/editor\/symbol/.test(o.path),
+            o => fakeResponse(404),
+          ],
+        ]);
+
+        it('returns a promise that resolve with the returned hover data', () => {
+          return waitsForPromise(() => KiteAPI.getReportDataAtPosition(filename, source, 18))
+          .then(data => {
+            const editorHash = md5(source);
+            const parsedURL = url.parse(KiteConnector.client.request.getCall(0).args[0].path);
+
+            expect(parsedURL.path.indexOf(filename.replace(/\//g, ':'))).not.to.eql(-1);
+            expect(parsedURL.path.indexOf(editorHash)).not.to.eql(-1);
+            const params = parseParams(parsedURL.query);
+
+            expect(params.cursor_runes).to.eql('18');
+            expect(data).to.eql([{
+              symbol: [{
+                id: 'foo',
+                value: [],
+              }],
+            }]);
+          });
+        });
+      });
+
+      describe('when both the hover request and the report request succeeds', () => {
+        withKiteRoutes([
+          [
+            o => /^\/api\/buffer\/atom/.test(o.path),
+            o => fakeResponse(200, JSON.stringify({
+              symbol: [{
+                id: 'foo',
+                value: [],
+              }],
+            })),
+          ], [
+            o => /^\/api\/editor\/symbol/.test(o.path),
+            o => fakeResponse(200, '{"bar": "foo"}'),
+          ],
+        ]);
+
+        it('returns a promise that resolve with both the returned report data', () => {
+          return waitsForPromise(() => KiteAPI.getReportDataAtPosition(filename, source, 18))
+          .then(data => {
+            const parsedURL = url.parse(KiteConnector.client.request.lastCall.args[0].path);
+            expect(parsedURL.path.indexOf('/foo')).not.to.eql(-1);
+
+            expect(data).to.eql([
+              {
+                symbol: [{
+                  id: 'foo',
+                  value: [],
+                }],
+              },
+            {bar: 'foo'},
+            ]);
+          });
+        });
+      });
+
+      describe('when the hover request fails', () => {
+        withKiteRoutes([
+          [
+            o => /^\/api\/buffer\/atom/.test(o.path),
+            o => fakeResponse(404),
+          ],
+        ]);
+
+        it('returns a rejected promise', () => {
+          return waitsForPromise({shouldReject: true}, () => KiteAPI.getReportDataAtPosition(filename, source, 18));
+        });
+      });
+    });
+
+    describe('.getValueReportDataForId()', () => {
+      describe('when the request succeeds', () => {
+        withKiteRoutes([[
+          o => /^\/api\/editor\/value/.test(o.path),
+          o => fakeResponse(200, '{"foo": "bar"}'),
+        ]]);
+
+        it('returns a promise that resolve with the returned hover data', () => {
+          return waitsForPromise(() => KiteAPI.getValueReportDataForId('foo'))
+          .then(data => {
+            const parsedURL = url.parse(KiteConnector.client.request.lastCall.args[0].path);
+
+            expect(parsedURL.path.indexOf('/foo')).not.to.eql(-1);
+
+            expect(data).to.eql({foo: 'bar'});
+          });
+        });
+
+        describe('when the response value does not have an id', () => {
+          withKiteRoutes([[
+            o => /^\/api\/editor\/value/.test(o.path),
+            o => fakeResponse(200, '{"foo": "bar", "value": {}}'),
+          ]]);
+
+          it('sets the value id using the provided one', () => {
+            return waitsForPromise(() => KiteAPI.getValueReportDataForId('foo'))
+            .then(data => {
+              expect(data.value.id).to.eql('foo');
+            });
+          });
+        });
+      });
+
+      describe('when the request fails', () => {
+        withKiteRoutes([[
+          o => /^\/api\/editor\/value/.test(o.path),
+          o => fakeResponse(404),
+        ]]);
+
+        it('returns a promise that is rejected', () => {
+          return waitsForPromise({shouldReject: true}, () => KiteAPI.getValueReportDataForId('foo'));
+        });
+      });
+    });
+
+    describe('.getMembersDataForId()', () => {
+      describe('when the request succeeds', () => {
+        withKiteRoutes([[
+          o => /^\/api\/editor\/value\/[^\/]+\/members/.test(o.path),
+          o => fakeResponse(200, '{"foo": "bar"}'),
+        ]]);
+
+        it('returns a promise that resolve with the returned members data', () => {
+          return waitsForPromise(() => KiteAPI.getMembersDataForId('foo'))
+          .then(data => {
+            const parsedURL = url.parse(KiteConnector.client.request.getCall(0).args[0].path);
+
+            expect(parsedURL.path.indexOf('/foo')).not.to.eql(-1);
+
+            expect(data).to.eql({foo: 'bar'});
+          });
+        });
+      });
+
+      describe('when the request fails', () => {
+        withKiteRoutes([[
+          o => /^\/api\/editor\/value\/[^\/]*\/members/.test(o.path),
+          o => fakeResponse(404),
+        ]]);
+
+        it('returns a promise that is rejected', () => {
+          return waitsForPromise({shouldReject: true}, () => KiteAPI.getMembersDataForId('foo'));
+        });
+      });
+    });
+
+    describe('.getUsagesDataForValueId()', () => {
+      describe('when the request succeeds', () => {
+        withKiteRoutes([[
+          o => /^\/api\/editor\/value\/[^\/]+\/usages/.test(o.path),
+          o => fakeResponse(200, '{"foo": "bar"}'),
+        ]]);
+
+        it('returns a promise that resolve with the returned members data', () => {
+          return waitsForPromise(() => KiteAPI.getUsagesDataForValueId('foo'))
+          .then(data => {
+            const parsedURL = url.parse(KiteConnector.client.request.getCall(0).args[0].path);
+
+            expect(parsedURL.path.indexOf('/foo')).not.to.eql(-1);
+
+            expect(data).to.eql({foo: 'bar'});
+          });
+        });
+      });
+
+      describe('when the request fails', () => {
+        withKiteRoutes([[
+          o => /^\/api\/editor\/value\/[^\/]*\/usages/.test(o.path),
+          o => fakeResponse(404),
+        ]]);
+
+        it('returns a promise that is rejected', () => {
+          return waitsForPromise({shouldReject: true}, () => KiteAPI.getUsagesDataForValueId('foo'));
+        });
+      });
+    });
+
+    describe('.getUsageDataForId()', () => {
+      describe('when the request succeeds', () => {
+        withKiteRoutes([[
+          o => /^\/api\/editor\/usages/.test(o.path),
+          o => fakeResponse(200, '{"foo": "bar"}'),
+        ]]);
+
+        it('returns a promise that resolve with the returned usage data', () => {
+          return waitsForPromise(() => KiteAPI.getUsageDataForId('foo'))
+          .then(data => {
+            const parsedURL = url.parse(KiteConnector.client.request.getCall(0).args[0].path);
+
+            expect(parsedURL.path.indexOf('/foo')).not.to.eql(-1);
+
+            expect(data).to.eql({foo: 'bar'});
+          });
+        });
+
+        describe('when the response value does not have an id', () => {
+          withKiteRoutes([[
+            o => /^\/api\/editor\/usages/.test(o.path),
+            o => fakeResponse(200, '{"foo": "bar", "value": {}}'),
+          ]]);
+
+          it('sets the value id using the provided one', () => {
+            return waitsForPromise(() => KiteAPI.getUsageDataForId('foo'))
+            .then(data => {
+              expect(data.value.id).to.eql('foo');
+            });
+          });
+        });
+      });
+
+      describe('when the request fails', () => {
+        withKiteRoutes([[
+          o => /^\/api\/editor\/usages/.test(o.path),
+          o => fakeResponse(404),
+        ]]);
+
+        it('returns a promise that is rejected', () => {
+          return waitsForPromise({shouldReject: true}, () => KiteAPI.getUsageDataForId('foo'));
+        });
+      });
+    });
+
+    describe('.getExampleDataForId()', () => {
+      describe('when the request succeeds', () => {
+        withKiteRoutes([[
+          o => /^\/api\/python\/curation/.test(o.path),
+          o => fakeResponse(200, '{"foo": "bar"}'),
+        ]]);
+
+        it('returns a promise that resolve with the returned example data', () => {
+          return waitsForPromise(() => KiteAPI.getExampleDataForId('foo'))
+          .then(data => {
+            const parsedURL = url.parse(KiteConnector.client.request.getCall(0).args[0].path);
+
+            expect(parsedURL.path.indexOf('/foo')).not.to.eql(-1);
+
+            expect(data).to.eql({foo: 'bar'});
+          });
+        });
+      });
+
+      describe('when the request fails', () => {
+        withKiteRoutes([[
+          o => /^\/api\/python\/curation/.test(o.path),
+          o => fakeResponse(404),
+        ]]);
+
+        it('returns a promise that is rejected', () => {
+          return waitsForPromise({shouldReject: true}, () => KiteAPI.getExampleDataForId('foo'));
+        });
+      });
+    });
+
+    describe('.getUserAccountInfo()', () => {
+      describe('when the request succeeds', () => {
+        withKiteRoutes([[
+          o => /^\/api\/account\/user/.test(o.path),
+          o => fakeResponse(200, '{"foo": "bar"}'),
+        ]]);
+
+        it('returns a promise that resolve with the returned example data', () => {
+          return waitsForPromise(() => KiteAPI.getUserAccountInfo())
+          .then(data => {
+            expect(data).to.eql({foo: 'bar'});
+          });
+        });
+      });
+
+      describe('when the request fails', () => {
+        withKiteRoutes([[
+          o => /^\/api\/account\/user/.test(o.path),
+          o => fakeResponse(404),
+        ]]);
+
+        it('returns a promise that is rejected', () => {
+          return waitsForPromise({shouldReject: true}, () => KiteAPI.getUserAccountInfo());
         });
       });
     });
